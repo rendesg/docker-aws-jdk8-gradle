0567b32500edd17dc0d3878ab7accd2dd6df0c48
rendesg <gabor.rendes@chilisoft.org>
2018-04-19 15:59:46 +0200

    updating dockerd-entrypoint.sh according to aws-codebuild dockerfile

--- dockerd-entrypoint.sh
+++ dockerd-entrypoint.sh
@@ -1,23 +1,23 @@
 #!/bin/sh
 set -e
 
-# no arguments passed
-# or first arg is `-f` or `--some-option`
-if [ "$#" -eq 0 -o "${1#-}" != "$1" ]; then
-	# add our default arguments
-	set -- dockerd \
-		--host=unix:///var/run/docker.sock \
-		--host=tcp://0.0.0.0:2375 \
-		"$@"
-fi
+/usr/local/bin/dockerd \
+	--host=unix:///var/run/docker.sock \
+	--host=tcp://0.0.0.0:2375 \
+	--storage-driver=overlay &>/var/log/docker.log &
 
-if [ "$1" = 'dockerd' ]; then
-	# if we're running Docker, let's pipe through dind
-	# (and we'll run dind explicitly with "sh" since its shebang is /bin/bash)
-	set -- sh "$(which dind)" "$@"
 
-	# explicitly remove Docker's default PID file to ensure that it can start properly if it was stopped uncleanly (and thus didn't clean up the PID file)
-	rm -f /var/run/docker.pid
-fi
+tries=0
+d_timeout=60
+until docker info >/dev/null 2>&1
+do
+	if [ "$tries" -gt "$d_timeout" ]; then
+                cat /var/log/docker.log
+		echo 'Timed out trying to connect to internal docker host.' >&2
+		exit 1
+	fi
+        tries=$(( $tries + 1 ))
+	sleep 1
+done
 
-exec "$@"
+eval "$@"
